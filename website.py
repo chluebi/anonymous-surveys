952d4479caf685aeb4ab4b2daa844d9ec6167be6
chluebi <42419603+chluebi@users.noreply.github.com>
2023-03-20 12:46:46 +0100

    getting state nonces to work

--- website.py
+++ website.py
@@ -1,5 +1,7 @@
 import json
 import os
+import secrets
+import time
 
 import requests
 import urllib.parse
@@ -28,22 +30,109 @@ for folder in os.listdir('data'):
         if schema['url'] in schemas:
             raise Exception('conflicting URLs')
         
+        schema['folder'] = folder
         schemas[schema['url']] = schema
 
 
+def prune_state_nonces(file):
+    with open(file, 'r') as f:
+        results = json.load(f)
+
+    nonces = results['nonces']
+    current_time = time.time()
+    new_nonces = list(filter(lambda n: n['expires'] > current_time, nonces))
+
+    results['nonces'] = new_nonces
+
+    with open(file, 'w') as f:
+        json.dump(results, f, indent=4)
+
+
+def add_state_nonce(file, nonce):
+    with open(file, 'r') as f:
+        results = json.load(f)
+
+    results['nonces'].append(nonce)
+
+    with open(file, 'w') as f:
+        json.dump(results, f, indent=4)
+
+
+def get_state_nonce(file, state):
+    with open(file, 'r') as f:
+        results = json.load(f)
+
+    found_nonce = None
+    for nonce in results['nonces']:
+        if nonce['state'] == state:
+            found_nonce = nonce
+            break
+
+    return found_nonce
+
+@app.route('/')
+def main():
+    code = request.args.get('code', default=None)
+    state = request.args.get('state', default=None)
+
+    if code and state:
+        correct_schema = None
+        for _, schema in schemas.items():
+            folder_path = 'data/' + schema['folder']
+            results_file = f'{folder_path}/results.json'
+
+            prune_state_nonces(results_file)
+            if get_state_nonce(results_file, state) is not None:
+                correct_schema = schema
+                break
+
+        if correct_schema is None:
+            return 'Invalid state'
+
+        API_ENDPOINT = 'https://discord.com/api/v10'
+        data = {
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': URL
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        r = requests.post(f'{API_ENDPOINT}/oauth2/token', data=data, headers=headers)
+        
+        r.raise_for_status()
+        access = r.json()
+        
+        access_token = access['access_token']
+        print(access_token)
+        return 'lol'
+    else:
+        return 'Nothing here'
+
+
 @app.route('/<name>')
-def main(name):
+def named(name):
+    print(schemas)
     if name not in schemas:
         return 'Survey not found'
     
     schema = schemas[name]
 
     code = request.args.get('code', default=None)
-    state = request.args.get('code', default=None)
+    state = request.args.get('state', default=None)
+
+    folder_path = 'data/' + schema['folder']
+    results_file = f'{folder_path}/results.json'
 
     named_url = f'{URL}/{name}'
 
     if code and state:
+        prune_state_nonces(results_file)
+        if get_state_nonce(results_file, state) is None:
+            return "Invalid state"
+
         API_ENDPOINT = 'https://discord.com/api/v10'
         data = {
             'client_id': CLIENT_ID,
@@ -61,8 +150,17 @@ def main(name):
         access = r.json()
         
         access_token = access['access_token']
+        print(access_token)
     else:
-        return render_template('index.html', url=urllib.parse.quote(named_url), client_id=CLIENT_ID, state='newyork')
+        state = secrets.token_urlsafe(16)
+        state_nonce = {
+            'state':state,
+            'expires':time.time()+(60*60) # one hour
+        }
+        prune_state_nonces(results_file)
+        add_state_nonce(results_file, state_nonce)
+
+        return render_template('index.html', url=urllib.parse.quote(URL), client_id=CLIENT_ID, state=state)
 
 
 app.run(port=36666, debug=True)
